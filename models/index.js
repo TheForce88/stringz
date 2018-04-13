46a355e7de949b9f4dd2d1501f0e6d5eb4f7930b
eches <Elaine.Chesoni83@gmail.com>
2018-04-13 01:04:42 -0700

    finishing touches

--- models/index.js
+++ models/index.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/newstringz', { promiseLibrary: global.Promise });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/newstringz');
+
+// heroku
+// mongoose.connect('mongodb://localhost/newstringz', { promiseLibrary: global.Promise });
 
 module.exports.User = require("./User");
 module.exports.Racquet = require("./Racquet");
